91f51a1331cf87cccad73f211a007b6a22ff46dd
Cindy Telmon <telcindy59@gmail.com>
2024-03-19 15:03:45 +0100

    fix

--- App.jsx
+++ App.jsx
@@ -5,9 +5,9 @@ import axios from 'axios'
 import { BaseUrl } from './util'
 
 export default function App() {
-    const [latitude, setLatitude] = useState(null)
-    const [longitude, setLongitude] = useState(null)
-    const [location, setLocation] = useState(null)
+    const [latitude, setLatitude] = useState('')
+    const [longitude, setLongitude] = useState('')
+    const [location, setLocation] = useState({ })
 
     useEffect(() => {
         Geolocation.getCurrentPosition(
@@ -19,34 +19,46 @@ export default function App() {
         );
     }, []);
 
-    useEffect(() => {
-        const Actualise = () => {
+//     useEffect(() => {
+//         const Actualise = () => {
+//         axios.get(`${BaseUrl}&q=${latitude},${longitude}`)
+//                 .then(response => {
+//                     console.log('Données de localisation reçues :', response.data);
+//                     setLocation(response.data);
+//                 })
+//                 .catch(error => {
+//                     console.error('Erreur lors de la récupération de la localisation :', error);
+//                 });
+//                 if (latitude !== null && longitude !== null) {
+//                     Actualise();
+//                 }
+// }},[latitude, longitude]);
+    function Actualise() {
+        console.log("press");
         axios.get(`${BaseUrl}&q=${latitude},${longitude}`)
                 .then(response => {
                     console.log('Données de localisation reçues :', response.data);
                     setLocation(response.data);
+                    console.log(location)
                 })
                 .catch(error => {
                     console.error('Erreur lors de la récupération de la localisation :', error);
                 });
-                if (latitude !== null && longitude !== null) {
-                    Actualise();
-                }
-}},[latitude, longitude]);
+    }
    
 
   return (
     <View>
        <Text>Latitude : {latitude}</Text>
             <Text>Longitude : {longitude}</Text>
-            {location && (
+           
                 <View>
                     <Text>Vous êtes :</Text>
-                    <Text>Ville : {location.LocalizedName}</Text>
-                    <Text>Pays : {location.Country.LocalizedName}</Text>
+                    {/* <Text>Ville : {location.LocalizedName}</Text>
+                    <Text>Pays : {location.Country.LocalizedName}</Text> */}
                     <Button title='Actualiser' onPress={Actualise}/>
                 </View>
-            )}
+          
     </View>
   )
 }

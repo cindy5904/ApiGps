0f176d99786862b55c5fff92c70000949acee559
Cindy Telmon <telcindy59@gmail.com>
2024-03-19 14:11:37 +0100

    exoApi

---
+++ App.jsx
@@ -0,0 +1,59 @@
+import { StyleSheet, Text, View, Button } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import Geolocation from '@react-native-community/geolocation'
+import axios from 'axios'
+import { BaseUrl } from './util'
+
+export default function App() {
+    const [latitude, setLatitude] = useState(null)
+    const [longitude, setLongitude] = useState(null)
+    const [location, setLocation] = useState(null)
+
+    useEffect(() => {
+        Geolocation.getCurrentPosition(
+            position => {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);
+            },
+            error => console.error(error)
+        );
+    }, []);
+
+    // useEffect(() => {
+    //     axios.get(`${BaseUrl}&q=${latitude},${longitude}`)
+    //             .then(response => {
+    //                 console.log('Données de localisation reçues :', response.data);
+    //                 setLocation(response.data);
+    //             })
+    //             .catch(error => {
+    //                 console.error('Erreur lors de la récupération de la localisation :', error);
+    //             });
+    //     },[latitude, longitude]);
+        function Actualise() {
+            axios.get(`${BaseUrl}&q=${latitude},${longitude}`)
+            .then(response => {
+                console.log('Données de localisation :', response.data);
+                setLocation(response.data);
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération de la localisation :', error);
+            });
+    }
+
+  return (
+    <View>
+       <Text>Latitude : {latitude}</Text>
+            <Text>Longitude : {longitude}</Text>
+            {location && (
+                <View>
+                    <Text>Vous êtes :</Text>
+                    <Text>Ville : {location.LocalizedName}</Text>
+                    <Text>Pays : {location.Country.LocalizedName}</Text>
+                    <Button title='Actualiser' onPress={Actualise}/>
+                </View>
+            )}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ util.js
@@ -0,0 +1,2 @@
+export const key = '8L49DXTQ0WSLxUQ1BdHGAWJJjkN8CI1t'
+export const BaseUrl = `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${key}`
